753b77d0ec3046ee9d7c071b82b78c496c4700e8
sigmamonstr <jchen85@gmail.com>
2018-12-27 14:54:31 -0500

    First commit for chapter 4

---
+++ chapter4/DIY-write_own_function.R
@@ -0,0 +1,44 @@
+
+library(RCurl)
+library(XML)
+
+google.counts <- function(search.term){
+  #
+  # Retrieve number of search results for exact query
+  #
+  # Args:
+  
+  #Construct search url
+  search.url <- paste("http://www.google.com/search?q=",  
+                      '"',  gsub(" ", "+", search.term), '"',
+                      sep = "")
+  
+  #Get URL from Google
+  search.html <- getURL(search.url)
+  
+  #Parse HTML
+  parse.search <- htmlTreeParse(search.html, useInternalNodes = TRUE)
+  
+  #Extract result statistics
+  nodes <- getNodeSet(parse.search, "//div[@id='resultStats']")
+  value <- strsplit(xmlValue(nodes[[1]])," ", fixed = TRUE)[[1]][2]
+  return(as.numeric(gsub(",", "", value, fixed = TRUE)))
+  
+}
+
+##Example
+  term <- "government shutdown"
+  results <- data.frame() #log for the results
+
+#Loop through
+for( i in 2004:2019){
+  keyword <- paste(term, i)
+  results <- rbind(results,
+                   data.frame(year = i, 
+                              cnt = google.counts(keyword)))
+  print(keyword)
+}
+
+#Plot
+  plot(results, type = "l")
+  
